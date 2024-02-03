89db0aace51d687a43f114487bb1fa269453e805
abhi067 <96076441+abhi067@users.noreply.github.com>
2024-02-03 23:40:24 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,32 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider} from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore, doc, setDoc } from "firebase/firestore";
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAHExHfMDRu2lVecgYxgW86TXDF-dvsWrY",
-  authDomain: "personal-finance-tracker-ec56e.firebaseapp.com",
-  projectId: "personal-finance-tracker-ec56e",
-  storageBucket: "personal-finance-tracker-ec56e.appspot.com",
-  messagingSenderId: "669303047696",
-  appId: "1:669303047696:web:5b4b41cc896752c03e6c95",
-  measurementId: "G-7X36E69HYW",
+  apiKey: "AIzaSyDqDuHBtzpTUz15pszYHZHnfW8d5lsPBC8",
+  authDomain: "financely-c4f84.firebaseapp.com",
+  projectId: "financely-c4f84",
+  storageBucket: "financely-c4f84.appspot.com",
+  messagingSenderId: "628987313883",
+  appId: "1:628987313883:web:a994affaa4eebc539c56fe",
+  measurementId: "G-8H86PS4CCV"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
 export { db, auth, provider, doc, setDoc };
